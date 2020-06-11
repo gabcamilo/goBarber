497e84a13a1fc138dec7095b4897506134050dbe
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-06-11 17:09:04 -0300

    Registered the database on the application.

--- src/app/models/Users.js
+++ src/app/models/Users.js
@@ -2,6 +2,7 @@ import Sequelize, { Model } from 'sequelize';
 
 class User extends Model {
 	static init(sequelize) {
+		// 'sequelize' contains the connection
 		super.init(
 			{
 				name: Sequelize.STRING,
