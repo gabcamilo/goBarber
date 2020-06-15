44d128cfd8bff0007b43d47cb0736c5034566111
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-06-15 12:28:36 -0300

    Used bcrypt and sequelize hook to encrypt users password.

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
 	static init(sequelize) {
@@ -7,6 +8,7 @@ class User extends Model {
 			{
 				name: Sequelize.STRING,
 				email: Sequelize.STRING,
+				password: Sequelize.VIRTUAL,
 				password_hash: Sequelize.STRING,
 				provider: Sequelize.BOOLEAN,
 			},
@@ -14,6 +16,19 @@ class User extends Model {
 				sequelize,
 			}
 		);
+
+		/* Sequelize 'hook' functionality:
+		 * add a specific code that executes automatically based on model actions.
+		 */
+
+		this.addHook('beforeSave', async (user) => {
+			if (user.password) {
+				user.password_hash = await bcrypt.hash(user.password, 8);
+			}
+
+			// returns the model
+			return this;
+		});
 	}
 }
 
